1d08cbaaed9cc67079418a3d4cfb1392fd194b20
Tommy McGahee <tommy.mcgahee@usg.edu>
2015-09-17 10:31:19 -0400

    first commit of puppet master foreman branch.

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -2,33 +2,19 @@ if which puppet > /dev/null 2>&1; then
   echo 'Puppet Installed.'
 else
   echo 'Installing Puppet Master...'
-  rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-11.noarch.rpm
-  yum --nogpgcheck -y install puppet-server
+  rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+  yum -y install epel-release http://yum.theforeman.org/releases/1.9/el6/x86_64/foreman-release.rpm
+  yum -y install foreman-installer
+  foreman-installer
   
-  # Configure puppet.conf and cert autosign
-  #puppet config set --section main modulepath  /etc/puppet/modules
-  puppet config set --section main environmentpath  \$confdir/environments
-  puppet config set --section master autosign  true
-  echo '*.development.vbox' > /etc/puppet/autosign.conf
+  sudo /usr/bin/puppet resource service iptables ensure=stopped enable=false
   
-  # Set hostnames and IPs
-  echo '192.168.50.2  puppetmaster.development.vbox  puppetmaster' | sudo tee --append /etc/hosts 2> /dev/null && \
-  echo '192.168.50.3  tommys-mbp.development.vbox    tommys-mbp'   | sudo tee --append /etc/hosts 2> /dev/null && \
-  
-  # Make dev env & transfer site.pp; transfer custom global role, profile and modified nodejs modules (all included in project repo)
-  mkdir -p /etc/puppet/environments/development/{manifests,modules}
-  cp /vagrant/development/manifests/site.pp /etc/puppet/environments/development/manifests
-  cp -r /vagrant/modules/{role,profile,custom_node_npm} /etc/puppet/modules
-    
   # Install shared modules from Puppet Forge
-  sudo puppet module install -i /etc/puppet/modules puppetlabs-ntp
-  sudo puppet module install -i /etc/puppet/modules treydock-gpg_key    
-  sudo puppet module install -i /etc/puppet/modules puppetlabs-apache
-  sudo puppet module install -i /etc/puppet/modules mayflower-php
+    sudo puppet module install -i /etc/puppet/modules puppetlabs-ntp
+    sudo puppet module install -i /etc/puppet/modules treydock-gpg_key    
+    sudo puppet module install -i /etc/puppet/modules puppetlabs-apache
+    sudo puppet module install -i /etc/puppet/modules mayflower-php
   
-  sudo chmod 444 /etc/puppet/modules/yum/metadata.json
-  
-  /usr/bin/puppet resource service iptables ensure=stopped enable=false
-  /usr/bin/puppet resource service puppetmaster ensure=running enable=true  
-  
+    sudo chmod 444 /etc/puppet/modules/yum/metadata.json
+   
 fi
