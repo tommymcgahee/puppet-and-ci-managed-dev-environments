4d190f0e35dc85438257e1023cc6ad289b693d50
tmcgahee <tommy.mcgahee@gmail.com>
2015-10-03 22:46:40 -0400

    changes to bootstrap

--- bootstrap-agent.sh
+++ bootstrap-agent.sh
@@ -14,7 +14,6 @@ else
   # Allow Foreman to trigger Puppet runs using Puppet kick (deprecated in Puppet 4.0)
   puppet config set --section agent listen true
   sudo touch /etc/puppet/namespaceauth.conf # Can remain empty, needed for bug
-  
   sudo sed -i '/# deny everything else/i path /run\nauth any\nmethod save\nallow puppetmaster.development.vbox\n' /etc/puppet/auth.conf
   
   # Disable firewall for now
@@ -23,15 +22,13 @@ else
   # Set hostnames and IPs
   echo '192.168.50.2  puppetmaster.development.vbox  puppetmaster' | sudo tee --append /etc/hosts 2> /dev/null && \
   echo '192.168.50.3  tommys-mbp.development.vbox    tommys-mbp'   | sudo tee --append /etc/hosts 2> /dev/null && \
+  echo '192.168.50.3  tommys-mbp-2.development.vbox  tommys-mbp-2' | sudo tee --append /etc/hosts 2> /dev/null && \
   echo '192.168.50.3  dhcp-82-204.development.vbox   dhcp-82-204'  | sudo tee --append /etc/hosts 2> /dev/null && \
   echo '192.168.50.4  production.vbox   			 production'   | sudo tee --append /etc/hosts 2> /dev/null && \
   
   # Change directories to Grunt project folder for Vagrant user on shell start
-  echo "cd /vagrant/www/" >> /home/vagrant/.bash_profile
+  # echo "cd /vagrant/www/" >> /home/vagrant/.bash_profile
       
   # Start Puppet and request cert
-  sudo puppet resource service puppet ensure=running enable=true
-    
-  #npm install && bower install  	
-  
+  sudo puppet resource service puppet ensure=running enable=true  
 fi
