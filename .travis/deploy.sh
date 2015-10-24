cc14535e9a4c44587049c6a148254b5342f1e2be
Tommy McGahee <tommy.mcgahee@usg.edu>
2015-10-24 17:20:15 -0400

    working on CI deployment...

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,12 +2,12 @@
 
 cd dist
 git init
-eval "$(ssh-agent -s)"
-chmod 600 .travis/deploy_key 
-ssh-add .travis/deploy_key
+
 git remote add production deploy@tommymcgahee.com:interview
 git config --global push.default simple
 git config --global user.email "tommy.mcgahee@gmail.com"
 git config --global user.name "deploy"
+
+git add *
 git commit -m "Deployed to production server"
 git push --force --quiet production master > /dev/null
